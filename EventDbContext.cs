9ae8a0f712944441382ee73774e1d33b31af61cf
jgrissom <jgrissom91@gmail.com>
2024-10-28 20:19:29 -0500

    Update to dotnet 8

--- EventDbContext.cs
+++ EventDbContext.cs
@@ -1,37 +1,36 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-// using Microsoft.Extensions.Configuration.Json;
 
 public class EventDbContext : DbContext
 {
-public DbSet<Event> Events { get; set; }
-public DbSet<Location> Locations { get; set; }
-public void AddEvents(List<Event> events)
-{
+  public DbSet<Event> Events { get; set; }
+  public DbSet<Location> Locations { get; set; }
+  public void AddEvents(List<Event> events)
+  {
     this.Events.AddRange(events);
     this.SaveChanges();
-}
-public void AddLocations(List<Location> locations)
-{
+  }
+  public void AddLocations(List<Location> locations)
+  {
     this.Locations.AddRange(locations);
     this.SaveChanges();
-}
-public void AddEvent(Event evt)
-{
+  }
+  public void AddEvent(Event evt)
+  {
     this.Events.Add(evt);
     this.SaveChanges();
-}
-public void AddLocation(Location loc)
-{
+  }
+  public void AddLocation(Location loc)
+  {
     this.Locations.Add(loc);
     this.SaveChanges();
-}
-protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-{
-    var configuration =  new ConfigurationBuilder()
-        .AddJsonFile($"appsettings.json");
+  }
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    var configuration = new ConfigurationBuilder()
+      .AddJsonFile($"appsettings.json");
 
     var config = configuration.Build();
-    optionsBuilder.UseSqlServer(@config["EventDbContext:ConnectionString"]);
-}
+    optionsBuilder.UseSqlServer(@config["Events:ConnectionString"]);
+  }
 }
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Modas-Seed
-1. <code>git clone https://github.com/jgrissom/SeedData.git</code>
+
+1. <code>git clone https://github.com/jgrissom/SeedData.git .</code>
 2. Rename rename.appsettings.json to appsettings.json
 3. Insert valid connection info
-4. <code>dotnet ef migrations add initial</code>
+4. <code>dotnet ef migrations add InitialCreate</code>
 5. <code>dotnet ef database update</code>
